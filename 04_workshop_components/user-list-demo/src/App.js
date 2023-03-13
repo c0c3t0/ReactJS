9efe535909739fdf926986c721a7235b1a92b85f
c0c3t0 <98971277+c0c3t0@users.noreply.github.com>
2023-03-13 02:25:20 +0200

    Workshop: Components

--- 04_workshop_components/user-list-demo/src/App.js
+++ 04_workshop_components/user-list-demo/src/App.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 
-import { getAllUsers } from './services/userService.js';
+import { getAllUsers, createUser } from './services/userService.js';
 
 import './App.css';
 import Header from './components/Header';
@@ -9,7 +9,7 @@ import Footer from './components/Footer';
 import Users from './components/Users';
 
 function App() {
-const[users, setUsers] = useState([]);
+    const [users, setUsers] = useState([]);
 
     useEffect(() => {
         getAllUsers()
@@ -18,7 +18,19 @@ const[users, setUsers] = useState([]);
             }).catch((err) => {
                 console.error(err);
             });
-    }, [])
+    }, []);
+
+    const onUserCreate = async (e) => {
+        e.preventDefault();
+
+        const formData = new FormData(e.currentTarget);
+
+        const data = Object.fromEntries(formData);
+
+        const result = await createUser(data);
+
+        setUsers(state => [...state, result.user]);
+    }
 
     return (
         <>
@@ -26,7 +38,7 @@ const[users, setUsers] = useState([]);
             <main className="main">
                 <section className="card users-container">
                     <Search />
-                    <Users users={users}/>
+                    <Users users={users} onUserCreate={onUserCreate} />
                 </section>
             </main>
             <Footer />
--- 04_workshop_components/user-list-demo/src/services/userService.js
+++ 04_workshop_components/user-list-demo/src/services/userService.js
@@ -3,5 +3,40 @@ const baseUrl = 'http://localhost:3005/api/users';
 export async function getAllUsers() {
     const response = await fetch(baseUrl);
     const data = await response.json();
+
     return data.users;
-}
+};
+
+export async function getUserById(id) {
+    const response = await fetch(`${baseUrl}/${id}`);
+    const data = await response.json();
+
+    return data.user;
+};
+
+export async function createUser(userData) {
+    const {
+        country,
+        city,
+        street,
+        streetNumber,
+        ...data
+    } = userData;
+
+    data.address = {
+        country,
+        city,
+        street,
+        streetNumber
+    }
+
+    const response = await fetch(baseUrl, {
+        method: 'POST',
+        headers: {
+            'content-type': 'application/json',
+        },
+        body: JSON.stringify(data)
+    });
+
+    return await response.json();
+};
